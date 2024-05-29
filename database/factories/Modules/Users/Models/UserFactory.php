ccd5da6d0635cc6dd493dab5c5cee2f4b391d5ee
simba77 <simba7@inbox.ru>
2024-05-29 11:36:59 +0300

    Заменены миграции и обновлен сидер с фабрикой для создания юзера

--- database/factories/Modules/Users/Models/UserFactory.php
+++ database/factories/Modules/Users/Models/UserFactory.php
@@ -2,16 +2,28 @@
 
 declare(strict_types=1);
 
-namespace Database\Factories;
+namespace Database\Factories\Modules\Users\Models;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Modules\Users\Models\User;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Users\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    protected static function appNamespace(): string
+    {
+        return 'Modules\\Users\\Models\\';
+    }
+
     /**
      * Define the model's default state.
      *
@@ -23,7 +35,7 @@ class UserFactory extends Factory
             'name'              => fake()->name(),
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password'          => static::$password ??= Hash::make('password'),
             'remember_token'    => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        User::query()->create(
+        User::factory()->create(
             [
                 'name'     => 'admin',
-                'email'    => 'admin@admin.ru',
+                'email'    => 'admin@admin.com',
                 'password' => Hash::make('password'),
             ]
         );
