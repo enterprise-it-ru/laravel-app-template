267469f50f3ffacb3ff33621c6b198455d759300
simba77 <simba7@inbox.ru>
2024-05-29 11:14:28 +0300

    Фикс exit code для gh action

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,4 +9,11 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /**
+     * A basic test example.
+     */
+    public function testThatTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
 }
