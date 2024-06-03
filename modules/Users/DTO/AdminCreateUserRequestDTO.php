5f92bdd39d94d4528ec1bce2fc0596d634658419
simba77 <simba7@inbox.ru>
2024-06-03 11:34:29 +0300

    Добавлена проверка на уникальность email

--- modules/Users/DTO/AdminCreateUserRequestDTO.php
+++ modules/Users/DTO/AdminCreateUserRequestDTO.php
@@ -6,16 +6,18 @@ namespace Modules\Users\DTO;
 
 use Spatie\LaravelData\Attributes\Validation\Email;
 use Spatie\LaravelData\Attributes\Validation\Max;
+use Spatie\LaravelData\Attributes\Validation\Unique;
 use Spatie\LaravelData\Data;
 
 class AdminCreateUserRequestDTO extends Data
 {
     public function __construct(
         public bool $active,
-        #[Max(200)]
+        #[Max(255)]
         public string $name,
         #[Email]
-        #[Max(200)]
+        #[Max(255)]
+        #[Unique('users', 'email')]
         public string $email,
         public string $role,
         public string $password,
