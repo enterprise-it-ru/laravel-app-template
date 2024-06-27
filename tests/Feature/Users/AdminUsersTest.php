a470e4008a569b739dd43ee605745814a2f3adcb
simba77 <simba7@inbox.ru>
2024-06-27 14:07:29 +0300

    Доработан тест проверки доступа к страницам для управления юзерами

--- tests/Feature/Users/AdminUsersTest.php
+++ tests/Feature/Users/AdminUsersTest.php
@@ -6,16 +6,29 @@ namespace Tests\Feature\Users;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\Users\Models\User;
+use PHPUnit\Framework\Attributes\TestWith;
 use Tests\TestCase;
 
 class AdminUsersTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testAccessToPages(): void
+    #[TestWith(['admin.users.index'], 'index')]
+    #[TestWith(['admin.users.list-filters'], 'list-filters')]
+    #[TestWith(['admin.users.create', 'POST'], 'create')]
+    #[TestWith(['admin.users.update', 'POST'], 'update')]
+    #[TestWith(['admin.users.edit-user-form', 'GET', ['id' => 1]], 'edit-user-form')]
+    #[TestWith(['admin.users.get-form-params'], 'get-form-params')]
+    #[TestWith(['admin.users.delete', 'POST', ['id' => 1]], 'delete')]
+    public function testAccessToPages(string $routeName, string $method = 'GET', array $params = []): void
     {
-        $response = $this->get(route('admin.users.index'));
-        $response->assertRedirect('/');
+        if ($method === 'GET') {
+            $response = $this->get(route($routeName, $params));
+            $response->assertRedirect('/');
+        } elseif ($method === 'POST') {
+            $response = $this->post(route($routeName, $params));
+            $response->assertRedirect('/');
+        }
     }
 
     public function testIndexPage(): void
